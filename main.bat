b17a4be36e62ebaa973098d20a20dc15ce89585e
KHajili98 <68163357+KHajili98@users.noreply.github.com>
2024-05-21 12:22:45 -0700

    Update main.bat

--- main.bat
+++ main.bat
@@ -22,7 +22,7 @@ set PRINTER_URL=http://localhost:3000
 set DB_DEFAULT=postgres
 
 REM Run the backend server
-start python manage.py runserver 0.0.0.0:8000
+start cmd /k "python manage.py runserver 0.0.0.0:8000"
 
 REM Navigate back to the main folder
 cd ..
@@ -32,7 +32,7 @@ cd frontend
 npm install
 
 REM Run the frontend server
-start npm run serve
+start cmd /k "npm run serve"
 
 REM Navigate back to the main folder
 cd ..
@@ -42,7 +42,7 @@ cd printer-v2
 npm install
 
 REM Run the printer service
-start npm run start
+start cmd /k "npm run start"
 
 REM Navigate back to the main folder
 cd ..
