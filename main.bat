579f92cbc84be6536401de8001c5570d9c4dffef
zakiraghayev <agayevzakir239@gmail.com>
2024-05-21 23:50:32 +0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -11,81 +11,28 @@ cd ..
 
 REM Backend commands
 cd managements
-IF NOT EXIST "venv" (
-    echo Creating virtual environment...
-    python -m venv venv
-    if %ERRORLEVEL% neq 0 (
-        set errormessage=Failed to create virtual environment.
-        call :reportError
-    )
-) ELSE (
-    echo Virtual environment already exists. Skipping creation...
-)
-
-REM Activate virtual environment
-call .\venv\Scripts\activate
-if %ERRORLEVEL% neq 0 (
-    set errormessage=Failed to activate virtual environment.
-    call :reportError
-)
-
-REM Install required Python packages
-pip install -r requirements.txt
-if %ERRORLEVEL% neq 0 (
-    set errormessage=Failed to install Python packages.
-    call :reportError
-)
-
-REM Set environment variables
-set PRINTER_URL=http://localhost:3000
-set DB_DEFAULT=postgres
 
 REM Run the backend server in a new command window
-start "DJANGO" cmd /k "echo Starting backend server... && python manage.py runserver 0.0.0.0:8000 && pause"
-if %ERRORLEVEL% neq 0 (
-    set errormessage=Backend server failed to start.
-    call :reportError
-)
+start "DJANGO" cmd /k "cd /d %CD% && call .\venv\Scripts\activate && set PRINTER_URL=http://localhost:3000 && set DB_DEFAULT=postgres && python manage.py runserver 0.0.0.0:8000 && pause"
 
 REM Navigate back to the main folder
 cd ..
 
 REM Frontend commands
 cd frontend
-npm install
-if %ERRORLEVEL% neq 0 (
-    set errormessage=Failed to install npm packages.
-    call :reportError
-)
 
-REM Run the frontend server in a new command window
-start "FRONT" cmd /k "echo Starting frontend server... && npm run serve && pause"
-if %ERRORLEVEL% neq 0 (
-    set errormessage=Frontend server failed to start.
-    call :reportError
-)
+start "FRONT" cmd /k "cd /d %CD% && npm install && if !ERRORLEVEL! neq 0 (echo Failed to install npm packages. && pause && exit /b !ERRORLEVEL!) && npm run serve && pause"
 
 REM Navigate back to the main folder
 cd ..
 
 REM Printer service commands
 cd printer-v2
-npm install
-if %ERRORLEVEL% neq 0 (
-    set errormessage=Failed to install npm packages for printer service.
-    call :reportError
-)
 
-REM Run the printer service in a new command window
-start "PRINTER" cmd /k "echo Starting printer service... && npm run start && pause"
-if %ERRORLEVEL% neq 0 (
-    set errormessage=Printer service failed to start.
-    call :reportError
-)
+start "PRINTER" cmd /k "cd /d %CD% && npm install && if !ERRORLEVEL! neq 0 (echo Failed to install npm packages for printer service. && pause && exit /b !ERRORLEVEL!) && npm run start && pause"
 
 REM Navigate back to the main folder
 cd ..
 
 pause
-
 exit /b 0
